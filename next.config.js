12d1e30db386c4670e3b123b2c2bf495b3468f59
jkkim <wddoods015@gmail.com>
2024-11-09 16:01:42 +0900

    deploy test2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    toolbar: false,  // 툴바 비활성화
+  },
 };
 
 module.exports = nextConfig;
